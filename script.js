c54536f5f6853d9ee4e0503fe06b1f30d359a1ae
Jessica <jesspeters143@gmail.com>
2021-08-19 21:22:43 -0700

    Second commit. Added movies.html and fetch movies API

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ function getMoreInfo(){
       .then(data => {
         console.log(data)
         document.querySelector('h1').innerText = data.name
-        document.querySelector('h2').innerText = data.height
-        document.querySelector('h3').innerText = data.hair_color
-      })
+        document.querySelector('h2').innerText = "Height: " + data.height
+        })
       .catch(err => {
         console.log(`error ${err}`)
       });
-}
+ }
+
+ document.querySelector('button').addEventListener('click', getMovies)
+
+  function getMovies(){
+    const url = 'https://swapi.dev/api/films/1'
+
+    fetch(url)
+        .then(res => res.json()) //parse response as JSON
+        .then(data => {
+          console.log(data)
+          document.querySelector('h3').innerText = data.title
+          document.querySelector('p').innerText = data.opening_crawl
+          })
+        .catch(err => {
+          console.log(`error ${err}`)
+        });
+   }
